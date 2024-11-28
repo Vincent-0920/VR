1a21fd81be297d4c9d587ddd27551feedba29894
Vincent-0920 <vincent772000@gmail.com>
2024-11-28 12:01:13 +0800

    3

---
+++ get-into/data.js
@@ -0,0 +1,233 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "斜角巷",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -0.8225141311601583,
+        "pitch": 0.005746953011479761,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.28862991097869894,
+          "pitch": 0.2783522473317248,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5354340419325094,
+          "pitch": 0.018308106403967628,
+          "title": "斜角巷",
+          "text": "販賣所有魔法物品"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "古靈閣巫師銀行",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.416128181311553,
+        "pitch": -0.3231299009092474,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5829474142599862,
+          "pitch": 0.03896346054825983,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6826750488663045,
+          "pitch": -0.022349928325830604,
+          "title": "古靈閣巫師銀行",
+          "text": "除了霍格華茲以外最安全的地方"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "九又四分之三月台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.01923167801997394,
+        "pitch": 0.032539269165608786,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11035304335866414,
+          "pitch": 0.18016279234202415,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.9986085670386045,
+          "pitch": 0.27762415652374806,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.026098278885696047,
+          "pitch": 0.30948700570252186,
+          "title": "九又四分之三月台",
+          "text": "進入霍格華茲的關鍵"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "霍格華茲列車",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0897195598391374,
+          "pitch": 0.03409403815604506,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.0651090782257544,
+          "pitch": 0.1795668491644058,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8949289021652724,
+          "pitch": -0.18191331328414684,
+          "title": "霍格華茲列車",
+          "text": "魔法列車<div>進入霍格華茲的關鍵</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "霍格華茲",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5612064209810299,
+          "pitch": 0.6978089451712961,
+          "rotation": 3.9269908169872414,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "get into hogwarts",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ movie/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "電影場景",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.913111106164905,
+        "pitch": -0.23258101589365232,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.734887866576008,
+          "pitch": 0.3861381971960469,
+          "title": "電影場景",
+          "text": "電影場景"
+        }
+      ]
+    }
+  ],
+  "name": "movie view",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
